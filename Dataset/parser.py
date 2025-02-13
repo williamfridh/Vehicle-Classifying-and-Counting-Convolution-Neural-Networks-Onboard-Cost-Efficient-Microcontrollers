a271d93dd36bda08adf973f83a700856be8344cf
PontusAhlin <pontus.ahlin@live.se>
2025-02-13 15:12:00 +0100

    Added audio sounds to correct folders

--- Dataset/parser.py
+++ Dataset/parser.py
@@ -1,5 +1,4 @@
 
-import numpy as np
 import pandas as pd
 import os
 import shutil
@@ -8,29 +7,32 @@ import shutil
 
 #1. Check csv, and the label
 
-#2. Take file from the C:\Users\Pontu\OneDrive\Skrivbord\Dataset\unbalanced_train_segments_training_set_audio_formatted_and_segmented_downloads folder
+#2. Take file from the C:/Users/Pontu/OneDrive/Skrivbord/Dataset/unbalanced_train_segments_training_set_audio_formatted_and_segmented_downloads folder
 
 #3. Put the file into the correct labeled folder 
 
+# Change csv depending on 
 
+csv_file = 'esc50.csv'
 
-data = pd.read_csv('groundtruth_weak_label_training_set.csv')
+# Change the category
+label = 'door_wood_creaks'
 
-print(data.head())
+source = 'C:/Users/Pontu/OneDrive/Skrivbord/lib/Vehicle-Classifying-and-Counting-Convolution-Neural-Networks-Onboard-Cost-Efficient-Microcontrollers/Dataset/audio'
 
+dest = os.path.join('C:/Users/Pontu/OneDrive/Skrivbord/lib/Vehicle-Classifying-and-Counting-Convolution-Neural-Networks-Onboard-Cost-Efficient-Microcontrollers/Dataset/split_up_data/background_noise/'
+                    , label)
+
+data = pd.read_csv(csv_file)
 
 for index, row in data.iterrows():
-    # Change the label depending on what to 
-    if(row['label'] == 'Bus'):
+    if row['category'] == label:
         # Take the audio from source and put it in dest
-        filename = 'Y' + row['filename']
-        source = 'C:/Users/Pontu/OneDrive/Skrivbord/Dataset/unbalanced_train_segments_training_set_audio_formatted_and_segmented_downloads/'
-        destLabel = 'Bus'
-        dest = 'C:/Users/Pontu/OneDrive/Skrivbord/Dataset/' + destLabel
-
+        filename = row['filename']
         src_path = os.path.join(source, filename)
         dst_path = os.path.join(dest, filename)
 
+        
         if os.path.exists(src_path):  # Ensure source file exists
             shutil.move(src_path, dst_path)
             print(f"Moved: {src_path} → {dst_path}")
