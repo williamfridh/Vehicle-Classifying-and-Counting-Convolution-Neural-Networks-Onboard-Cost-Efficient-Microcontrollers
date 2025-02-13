825afb6b19344ae7f64f1e258784671bd0328958
PontusAhlin <pontus.ahlin@live.se>
2025-02-13 16:37:10 +0100

    Finished make-mfcc to being able to create mfcc based on a audio file

---
+++ LibrosaCpp/test/make-mfcc.h
@@ -0,0 +1,14 @@
+#ifndef MAKE-MFCC_H
+#define MAKE-MFCC_H
+
+#include <string>
+#include <vector>
+
+// Function prototypes
+std::string makeMfcc(std::string audio_source, int sr);
+
+std::string mfccToString(std::vector<std::vector<float>> mfcc_matrix);
+
+std::vector<float> parseAudio(std::string audio_source);
+
+#endif // MAKE-MFCC_H
